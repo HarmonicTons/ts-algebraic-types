118b742da7f85bfd850ec34e52ecc94e93f72576
HarmonicTons <roncin.thomas.l.l@gmail.com>
2021-02-18 13:42:23 +0000

    wip

--- src/algebraic-types.ts
+++ src/algebraic-types.ts
@@ -1,12 +1,12 @@
-import Ajv from "ajv";
-import R from "rambda";
-const ajv = new Ajv();
+import * as Ajv from "ajv";
+import * as R from "rambda";
+const ajv = new Ajv({ allErrors: true });
 
 type UnderscoredString<U extends string> = `__${U}`;
 // equivalent 'of' of F# (generic nominal type)
-type Of<T,U extends string> = T & { [key in UnderscoredString<U>]: true };
+type Of<T, U extends string> = T & { [key in UnderscoredString<U>]: true };
 
-type ValidationError = object;
+type ValidationError = string;
 type TypeGuard<T> = (value: unknown) => value is T;
 type Validator = (candidate: unknown) => readonly ValidationError[];
 type Create<T> = (value: unknown) => T | undefined;
@@ -15,7 +15,9 @@ type Cast<T> = (value: unknown) => T;
 const makeValidator = (schema: object): Validator => (candidate) => {
     const validate = ajv.compile(schema);
     const isValid = validate(candidate) as boolean;
-    return validate.errors ?? [];
+    return validate.errors?.map(e =>
+        e.dataPath === "" ? e.message : `${e.dataPath} ${e.message}`
+    ) ?? [];
 }
 const makeTypeGuard = <T>(validator: Validator): TypeGuard<T> => (candidate): candidate is T => {
     const errors = validator(candidate);
@@ -27,12 +29,19 @@ const makeCreate = <T>(typeGuard: TypeGuard<T>): Create<T> => (candidate) => {
     }
     return;
 }
+const candidateToString = (candidate: unknown, l: number = 50): string => {
+    const str = JSON.stringify(candidate);
+    if (str.length < l) {
+        return str;
+    }
+    return str.slice(0, l) + "...";
+}
 const makeCast = <T>(validator: Validator): Cast<T> => (candidate) => {
     const errors = validator(candidate);
     if (errors.length === 0) {
         return candidate as T;
     }
-    throw new Error(`CAST ERROR: candidate does not respect validator '${validator.name}': \n${errors.join("\n")}`);
+    throw new Error(`CAST ERROR: candidate ${candidateToString(candidate)} does not respect validator: \n${errors.join("\n")}`);
 }
 
 
@@ -49,44 +58,78 @@ const makeTypeHandlerFromSchema = <T>(schema: object) => {
     const validator = makeValidator(schema);
     return makeTypeHandlerFromValidator<T>(validator);
 }
-const mergeValidators = (...validators: Validator[]): Validator => (candidate) => 
-    R.flatten(R.map(validator => validator(candidate), validators));
+const mergeValidators = (...validators: Validator[]): Validator => (candidate) =>
+    R.uniq(R.flatten(R.map(validator => validator(candidate), validators)));
 const makeTypeHandlerFromValidators = <T>(...validators: Validator[]) => {
     const validator = mergeValidators(...validators);
-    return makeTypeHandlerFromValidator(validator);
+    return makeTypeHandlerFromValidator<T>(validator);
 }
 
 
-// simple type
-type PhoneNumber = Of<string,"PhoneNumber">;
-const phoneNumberSchema = {
-    type: "string",
-    pattern: "^[0-9]{10}$"
+
+
+export const main = () => {
+    // simple type
+    type PhoneNumber = Of<string, "PhoneNumber">;
+    const phoneNumberSchema = {
+        type: "string",
+        pattern: "^[0-9]{10}$"
+    }
+    const phoneNumberTypeHandler = makeTypeHandlerFromSchema<PhoneNumber>(phoneNumberSchema)
+
+    const phone: PhoneNumber = phoneNumberTypeHandler.cast("0674991883");
+
+
+    // combinaison de types 
+    type StringOfNumbers = Of<string, 'StringOfNumbers'>;
+    const stringOfNumbersSchema = {
+        type: "string",
+        pattern: "^[0-9]*$",
+    }
+    const stringOfNumbersTypeHandler = makeTypeHandlerFromSchema<StringOfNumbers>(stringOfNumbersSchema)
+
+    type String4To6 = Of<string, 'String4To6'>;
+    const validatorString4To6: Validator = (candidate) => {
+        const errors: string[] = [];
+        if (typeof candidate !== "string") {
+            errors.push("should be string");
+            return errors;
+        }
+        if (candidate.length !== 4 && candidate.length !== 6) {
+            errors.push("should be 4 or 6 characters");
+        }
+        return errors;
+    }
+    const string4To6SchemaTypeHandler = makeTypeHandlerFromValidator<StringOfNumbers>(validatorString4To6)
+
+    type TrainNumber = StringOfNumbers & String4To6;
+    const trainNumberTypeHandler = makeTypeHandlerFromValidators<TrainNumber>(
+        stringOfNumbersTypeHandler.validator,
+        string4To6SchemaTypeHandler.validator
+    )
+
+
+    const train: TrainNumber = trainNumberTypeHandler.cast("256899");
+
+
+    const trains: TrainNumber[] = [];
+
+
+    type UniqArray = Of<any[],"UniqArray">
+    const validatorUniqTrainNumbers: Validator = (candidate) => {
+        const errors: string[] = [];
+        if (!Array.isArray(candidate)) {
+            errors.push("should be array");
+            return errors;
+        }
+        const arrayCandidate = candidate as unknown[];
+        if (R.uniq(arrayCandidate).length !== arrayCandidate.length) {
+            errors.push("should be array of uniq values");
+        }
+        return errors;
+    }
+    const uniqTrainNumbersTypeHandler = makeTypeHandlerFromValidator<UniqArray>(validatorUniqTrainNumbers)
+
+    
+    const uniqTrainNumbers: UniqTrainNumbers = uniqTrainNumbersTypeHandler.cast(["23659"]);
 }
-const phoneNumberTypeHandler = makeTypeHandlerFromSchema<PhoneNumber>(phoneNumberSchema)
-
-const phone: PhoneNumber = phoneNumberTypeHandler.cast("0344290262");
-
-
-// combinaison de types 
-type StringOfNumbers = Of<string,'StringOfNumbers'>;
-const stringOfNumbersSchema = {
-    type: "string",
-    pattern: "^[0-9]*$"
-}
-const stringOfNumbersTypeHandler = makeTypeHandlerFromSchema<StringOfNumbers>(stringOfNumbersSchema)
-
-type String4To6 = Of<string,'String4To6'>;
-const string4To6Schema = {
-    type: "string",
-    minLength: "4",
-    maxLength: "6",
-}
-const string4To6SchemaTypeHandler = makeTypeHandlerFromSchema<StringOfNumbers>(string4To6Schema)
-
-type TrainNumber = StringOfNumbers & String4To6;
-const trainNumberTypeHandler = makeTypeHandlerFromValidators<TrainNumber>(
-    stringOfNumbersTypeHandler.validator,
-    string4To6SchemaTypeHandler.validator
-)
-
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {main} from "./algebraic-types"
+
+try {
+    main();
+    console.log("success")
+} catch (error) {
+    console.error(error);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
